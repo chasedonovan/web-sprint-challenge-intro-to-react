dc5b49dc72368240eb98bf8b3971cb81096a4be8
Chase Donovan <chase.donovan@aol.com>
2021-06-01 15:56:50 -0700

    eh

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Char = styled.div`
+  padding: 0.25em 2em;
+  margin: 1em auto;
+  width: 33%;
+  text-align: center;
+  border: 4px solid orange;
+  background-color: #debd9288;
+`;
+
+const Character = (props) => {
+  const { char } = props;
+
+  return (
+    <Char>
+      <h1>{char.name}</h1>
+    </Char>
+  );
+};
+
+export default Character;
